dba1177944ea7dfd1cd312f235cd720966069eed
sk1es <144143057+sk1ees@users.noreply.github.com>
2024-10-11 08:35:39 +0530

    working on patterns

---
+++ Patterns/Pattern1.java
@@ -0,0 +1,20 @@
+package basics.Patterns;
+
+import java.util.Scanner;
+
+public class Pattern1 {
+    public static void main(String[] args) {
+
+        Scanner sc =new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+
+        for (int i = 0; i <= N; i++) {
+            for (int j = 0; j <= N; j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Patterns/Pattern4.java
@@ -0,0 +1,12 @@
+package basics.Patterns;
+
+public class Pattern4 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
